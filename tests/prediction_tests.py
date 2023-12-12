71022138f9087734364c0c1f7c4b1373b250d57a
Wiem Ben Saber <wiem.bensaber@skeelfully.com>
2023-12-12 19:03:45 +0100

    edit

--- tests/prediction_tests.py
+++ tests/prediction_tests.py
@@ -1,18 +1,19 @@
 import unittest
 import requests
 
-class TestPredictions(unittest.TestCase):
 
+class TestPredictions(unittest.TestCase):
 
     def test_backend_prediction(self):
         response = requests.post("http://localhost:8081/predict", json={"data": "sample_data"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["predicted_genre"], "expected_prediction_backend")
+        self.assertEqual(response.json()("predicted_genre"), "expected_prediction_backend")
 
     def test_vgg19_backend_prediction(self):
         response = requests.post("http://localhost:8082/vgg", json={"data": "sample_data"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["predicted_genre"], "expected_prediction_vgg19")
+        self.assertEqual(response.json()("predicted_genre"), "expected_prediction_vgg19")
+
 
 if __name__ == '__main__':
     unittest.main()
