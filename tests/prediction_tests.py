58ec2d3b5b5da1ea696464b40f46918077b10011
Wiem Ben Saber <wiem.bensaber@skeelfully.com>
2023-12-12 22:04:07 +0100

    edit

--- tests/prediction_tests.py
+++ tests/prediction_tests.py
@@ -1,6 +1,8 @@
+import logging
 import unittest
 import requests
 import os
+
 
 class TestPredictions(unittest.TestCase):
 
@@ -9,6 +11,8 @@ class TestPredictions(unittest.TestCase):
         self.genres = ["pop", "jazz", "metal", "blues", "disco", "classical", "country", "hiphop", "reggae", "rock"]
 
     def test_backend_prediction(self):
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
         # Chemin vers un fichier audio existant sur votre système
         audio_file_path = 'Front-end/audio_to_predict.wav'
 
@@ -25,7 +29,7 @@ class TestPredictions(unittest.TestCase):
         self.assertIsNotNone(predicted_genre, "La clé 'predicted_genre' n'est pas présente dans la réponse JSON.")
         expected_genres = ["expected_prediction_vgg19"] + self.genres
         self.assertIn(predicted_genre, expected_genres, f"Prédiction inattendue : {predicted_genre}")
-        self.logger.info(f"Backend Prediction: {predicted_genre}")
+        logger.info(f"Backend Prediction: {predicted_genre}")
 
     def test_vgg19_backend_prediction(self):
         # Chemin vers un fichier audio existant sur votre système
@@ -46,7 +50,7 @@ class TestPredictions(unittest.TestCase):
         # Ajoutez la liste des genres à la liste des genres attendus
         expected_genres = ["expected_prediction_vgg19"] + self.genres
         self.assertIn(predicted_genre, expected_genres, f"Prédiction inattendue : {predicted_genre}")
-        self.logger.info(f"VGG19 Backend Prediction: {predicted_genre}")
+        logger.info(f"VGG19 Backend Prediction: {predicted_genre}")
 
 
 if __name__ == '__main__':
