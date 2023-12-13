d8428e92a0ac090d3584dc6a35bf472030dedeb2
Wiem Ben Saber <wiem.bensaber@skeelfully.com>
2023-12-13 12:26:06 +0100

    predict

--- tests/prediction_tests.py
+++ tests/prediction_tests.py
@@ -10,14 +10,14 @@ class TestPredictions(unittest.TestCase):
 
     def test_backend_prediction(self):
         # Chemin vers un fichier audio existant sur votre système
-        audio_file_path = 'Front-end/audio_to_predict.wav'
+        audio_file_path = self.get_front_end_file_content("http://127.0.0.1:8083")
 
         # Vérifiez que le fichier audio existe
         self.assertTrue(os.path.exists(audio_file_path), f"Le fichier audio {audio_file_path} n'existe pas.")
 
         # Chargez le fichier audio dans la requête POST
         with open(audio_file_path, 'rb') as audio_file:
-            files = {'audio_file': ('Front-end/audio_to_predict.wav', audio_file, 'audio/wav')}
+            files = {'audio_file': ('audio_to_predict.wav', audio_file, 'audio/wav')}
             response = requests.post("http://127.0.0.1:8081/predict", files=files)
 
         self.assertEqual(response.status_code, 200)
@@ -47,5 +47,10 @@ class TestPredictions(unittest.TestCase):
         expected_genres = ["expected_prediction_vgg19"] + self.genres
         self.assertIn(predicted_genre, expected_genres, f"Prédiction inattendue : {predicted_genre}")
 
+    def get_front_end_file_content(self, endpoint):
+        # Simule une requête depuis le front-end en récupérant le contenu du fichier audio
+        response = requests.get(endpoint)
+        response.raise_for_status()
+        return response.content
 if __name__ == '__main__':
     unittest.main()
